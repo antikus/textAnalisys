fcb8c7072de602c685a6cfd400b62376e6b9d307
Anatoli Kurtsevich <anatoli.kurtsevich@gmail.com>
2012-03-15 23:05:41 +0300

    clear the frame class, comment the old gate handler class

--- workspace/EmotionAnalysis/src/frame/MainFrame.java
+++ workspace/EmotionAnalysis/src/frame/MainFrame.java
@@ -1,11 +1,9 @@
-package frameEmotionAnalysis;
-
-import gate.creole.ResourceInstantiationException;
-import gateEmotionAnalysis.EmotionAnalisys;
+package frame;
 
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.io.File;
+
 import javax.swing.AbstractAction;
 import javax.swing.Action;
 import javax.swing.JFileChooser;
@@ -20,44 +18,35 @@ public class MainFrame extends JFrame {
 	private static final int WIDTH = 800; 
 	private static final int HEIGHT = 600; 
 	private JFileChooser fileChooser = null; 
-	private static EmotionAnalisys emotionAnalisysController;
+//	private static EmotionAnalisys emotionAnalisysController;
 	
 	public MainFrame() { 
-		super( "EmotionApplication" ); 
+		super( "GATE App" ); 
 		setSize( WIDTH, HEIGHT ); 
 		Toolkit kit = Toolkit.getDefaultToolkit(); 
 		setLocation((kit.getScreenSize().width - WIDTH)/2, (kit.getScreenSize().height - HEIGHT)/2); 
 		JMenuBar menuBar = new JMenuBar(); 
 		setJMenuBar( menuBar ); 
-		JMenu fileMenu = new JMenu( "" ); 
-		menuBar.add( fileMenu ); 
+		JMenu fileMenu = new JMenu("File"); 
+		menuBar.add(fileMenu); 
 		
-		Action openDocumentAction = new AbstractAction( "" ) { 
-			public void actionPerformed( ActionEvent event ) { 
+		Action openDocumentAction = new AbstractAction("open") { 
+			public void actionPerformed(ActionEvent event) { 
 				if ( fileChooser == null ) { 
 					fileChooser = new JFileChooser(); 
-					fileChooser.setCurrentDirectory( new File(".") ); 
+					fileChooser.setCurrentDirectory(new File(".")); 
 				} 
 				if (fileChooser.showOpenDialog(MainFrame.this) == JFileChooser.APPROVE_OPTION){
 					System.out.println(fileChooser.getSelectedFile());
-					//try {
-						//emotionAnalisysController.loadDocument(fileChooser.getSelectedFile());
-					//} catch (ResourceInstantiationException e) {
-						// TODO Auto-generated catch block
-					//	e.printStackTrace();
-					//}
-					
 				}
 			}
 		}; 
-		fileMenu.add( openDocumentAction ); 
-		//JFRAME.	
+		fileMenu.add( openDocumentAction );	
 	} 
 	
 	public static void main(String[] args) {
 		MainFrame frame = new MainFrame(); 
 		frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE ); 
-		frame.setVisible(true); 
-		emotionAnalisysController = new EmotionAnalisys();
+		frame.setVisible(true);
 	} 
 }
